cc16d8b5dccf6bbdc6cae786405e858b4ac5861c
Kyli <kylidboy@outlook.com>
2025-07-07 17:48:04 +0800

    feat: futures income history

--- src/endpoints/futures_account.rs
+++ src/endpoints/futures_account.rs
@@ -14,6 +14,8 @@ pub enum AccountInfoEP {
     // AccountInfoV3,
     #[endpoint(GET, UserData, url = "/fapi/v1/balance")]
     Balance,
+    #[endpoint(GET, UserData, url = "/fapi/v1/income")]
+    IncomeHistory,
 }
 
 // #[derive(Debug, Serialize, APIRequestInit, APIRequestToString)]
@@ -43,4 +45,82 @@ pub struct BalanceResponse {
 
 impl EndpointRequest for BalanceRequest {
     type Response = Vec<BalanceResponse>;
+}
+
+#[derive(Debug, Serialize, APIRequestInit, APIRequestToString)]
+#[serde(rename_all = "camelCase")]
+pub struct IncomeHistoryRequest {
+    pub symbol: Option<String>,
+    pub income_type: Option<IncomeType>,
+    pub start_time: Option<i64>,
+    pub end_time: Option<i64>,
+    pub page: Option<i32>,
+    pub limit: Option<u32>,
+    #[serde(flatten)]
+    pub base: BaseRequest,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub enum IncomeType {
+    #[serde(rename = "TRANSFER")]
+    Transfer,
+    #[serde(rename = "WELCOME_BONUS")]
+    WelcomeBonus,
+    #[serde(rename = "REALIZED_PNL")]
+    RealizedPnl,
+    #[serde(rename = "FUNDING_FEE")]
+    FundingFee,
+    #[serde(rename = "COMMISSION")]
+    Commission,
+    #[serde(rename = "INSURANCE_CLEAR")]
+    InsuranceClear,
+    #[serde(rename = "REFERRAL_KICKBACK")]
+    ReferralKickback,
+    #[serde(rename = "COMMISSION_REBATE")]
+    CommissionRebate,
+    #[serde(rename = "API_REBATE")]
+    ApiRebate,
+    #[serde(rename = "CONTEST_REWARD")]
+    ContestReward,
+    #[serde(rename = "CROSS_COLLATERAL_TRANSFER")]
+    CrossCollateralTransfer,
+    #[serde(rename = "OPTIONS_PREMIUM_FEE")]
+    OptionsPremiumFee,
+    #[serde(rename = "OPTIONS_SETTLE_PROFIT")]
+    OptionsSettleProfit,
+    #[serde(rename = "INTERNAL_TRANSFER")]
+    InternalTransfer,
+    #[serde(rename = "AUTO_EXCHANGE")]
+    AutoExchange,
+    #[serde(rename = "DELIVERED_SETTELMENT")]
+    DeliveredSettelment,
+    #[serde(rename = "COIN_SWAP_DEPOSIT")]
+    CoinSwapDeposit,
+    #[serde(rename = "COIN_SWAP_WITHDRAW")]
+    CoinSwapWithdraw,
+    #[serde(rename = "POSITION_LIMIT_INCREASE_FEE")]
+    PositionLimitIncreaseFee,
+    #[serde(rename = "STRATEGY_UMFUTURES_TRANSFER")]
+    StrategyUmfuturesTransfer,
+    #[serde(rename = "FEE_RETURN")]
+    FeeReturn,
+    #[serde(rename = "BFUSD_REWARD")]
+    BfusdReward,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct IncomeHistory {
+    pub symbol: Option<String>,
+    pub income_type: IncomeType,
+    pub income: String,
+    pub asset: String,
+    pub info: String,
+    pub time: i64,
+    pub tran_id: u64,
+    pub trade_id: Option<String>,
+}
+
+impl EndpointRequest for IncomeHistoryRequest {
+    type Response = Vec<IncomeHistory>;
 }
